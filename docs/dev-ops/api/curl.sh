21e450abc16fdc9d70b5100fcb5ec5b58ba3b9e9
wandering_4 <17891997260@139.com>
2025-07-18 02:12:12 +0800

    ip fix

--- docs/dev-ops/api/curl.sh
+++ docs/dev-ops/api/curl.sh
@@ -1,7 +1,7 @@
-curl http://117.72.115.188:11434/api/generate \
+curl http://49.234.8.125:11434/api/generate \
   -H "Content-Type: application/json" \
   -d '{
         "model": "deepseek-r1:1.5b",
-        "prompt": "1+1",
+        "prompt": "你是谁",
         "stream": false
       }'
