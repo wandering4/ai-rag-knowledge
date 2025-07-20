dc7cffc028e3e4a70971b93568e224566852bf0b
wandering_4 <17891997260@139.com>
2025-07-21 01:13:36 +0800

    rag向量库检索

--- xzf-dev-tech-app/src/main/java/com/sankuai/xzf/dev/tech/config/OllamaConfig.java
+++ xzf-dev-tech-app/src/main/java/com/sankuai/xzf/dev/tech/config/OllamaConfig.java
@@ -1,10 +1,16 @@
 package com.sankuai.xzf.dev.tech.config;
 
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.OllamaEmbeddingClient;
 import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class OllamaConfig {
@@ -17,6 +23,27 @@ public class OllamaConfig {
     @Bean
     public OllamaChatClient ollamaChatClient(OllamaApi ollamaApi) {
         return new OllamaChatClient(ollamaApi);
+    }
+
+    //工具类
+    @Bean
+    public TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+
+    @Bean
+    public SimpleVectorStore vectorStore(OllamaApi ollamaApi) {
+        //实例化本地向量库
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new SimpleVectorStore(embeddingClient);
+    }
+
+    @Bean
+    public PgVectorStore pgVectorStore(OllamaApi ollamaApi, JdbcTemplate jdbcTemplate) {
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new PgVectorStore(jdbcTemplate, embeddingClient);
     }
 
 }
---
+++ xzf-dev-tech-app/src/test/java/com/sankuai/xzf/dev/tech/test/RAGTest.java
@@ -0,0 +1,126 @@
+package com.sankuai.xzf.dev.tech.test;
+
+import com.alibaba.fastjson.JSON;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RAGTest {
+
+    @Resource
+    private OllamaChatClient ollamaChatClient;
+
+    @Resource
+    private TokenTextSplitter tokenTextSplitter;
+
+    @Resource
+    private SimpleVectorStore simpleVectorStore;
+
+    @Resource
+    private PgVectorStore pgVectorStore;
+
+
+    @Test
+    public void uploadTest() throws IOException {
+        String path="data/file.text";
+        ClassPathResource resource = new ClassPathResource(path);
+        if (!resource.exists()) {
+            throw new FileNotFoundException("文件未找到: " + resource.getPath());
+        }
+
+        TikaDocumentReader reader = new TikaDocumentReader(path);
+
+        //切割文件
+        List<Document> documents = reader.get();
+        List<Document> documentsSplitterList = tokenTextSplitter.apply(documents);
+
+        //数据去重
+        documents = documents.stream()
+                .filter(doc -> doc.getContent() != null && !doc.getContent().isEmpty())
+                .collect(Collectors.toMap(
+                        Document::getContent,  // 以内容作为去重依据
+                        doc -> doc,
+                        (existing, replacement) -> existing  // 保留第一个出现的文档
+                ))
+                .values()
+                .stream()
+                .peek(doc -> doc.getMetadata().put("knowledge", "知识库名称"))
+                .toList();
+
+
+
+        //打标
+        documents.forEach(doc -> doc.getMetadata().put("knowledge","知识库名称"));
+        documentsSplitterList.forEach(document -> {
+            log.info("分割后文档内容: {}", document.getContent());
+            document.getMetadata().put("knowledge", "知识库名称");
+        });
+
+        //存储
+        pgVectorStore.accept(documentsSplitterList);
+
+        log.info("上传完成");
+    }
+
+    @Test
+    public void chatTest(){
+
+        String message = "意大利面应该拌什么?";
+
+        String SYSTEM_PROMPT = """
+                Use the information from the DOCUMENTS section to provide accurate answers but act as if you knew this information innately.
+                If unsure, simply state that you don't know.
+                Another thing you need to note is that your reply must be in Chinese!
+                DOCUMENTS:
+                    {documents}
+                """;
+
+        SearchRequest request = SearchRequest.query(message).withTopK(5).withFilterExpression("knowledge == '知识库名称'").withSimilarityThreshold(0.8);
+
+        List<Document> documents = pgVectorStore.similaritySearch(request);
+        String documentsCollectors = documents.stream().map(Document::getContent).collect(Collectors.joining("、"));
+
+        Message ragMessage = new SystemPromptTemplate(SYSTEM_PROMPT).createMessage(Map.of("documents", documentsCollectors));
+
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(new UserMessage(message));
+        messages.add(ragMessage);
+
+        log.info("发送给大模型消息: {}", JSON.toJSONString(messages));
+
+        ChatResponse chatResponse = ollamaChatClient.call(new Prompt(messages, OllamaOptions.create().withModel("deepseek-r1:1.5b")));
+
+        log.info("测试结果:{}", JSON.toJSONString(chatResponse));
+
+    }
+
+
+}
